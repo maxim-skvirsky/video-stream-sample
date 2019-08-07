99eed3f5764eae6e2accfa7a93b27e0061b2299f
Maxim Skvirsky <maxim.skvirsky@ptts.com>
2019-08-07 11:54:35 +0300

    refactoring

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.get("/video", function(req, res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log("Listening on port 3000!");
+app.listen(8000, function() {
+  console.log("Listening on port 8000!");
 });
