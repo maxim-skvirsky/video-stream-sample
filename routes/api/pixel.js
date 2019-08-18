95804093b41eaa4652d265bb317b83922f295e74
Maxim Skvirsky <maxim.skvirsky@ptts.com>
2019-08-18 16:54:41 +0300

    request logging at pixels level

--- routes/api/pixel.js
+++ routes/api/pixel.js
@@ -2,57 +2,57 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/impression-pixel", function(req, res) {
-  console.log("impression-pixel get");
+  console.log("impression-pixel get " + req);
   res.status(200).send();
 });
 
 router.post("/impression-pixel", function(req, res) {
-  console.log("impression-pixel post");
+  console.log("impression-pixel post " + req);
 });
 
 router.get("/start", function(req, res) {
-  console.log("start get");
+  console.log("start get " + req);
   res.status(200).send();
 });
 
 router.post("/start", function(req, res) {
-  console.log("start post");
+  console.log("start post " + req);
 });
 
 router.get("/complete", function(req, res) {
-  console.log("complete get");
+  console.log("complete get " + req);
   res.status(200).send();
 });
 
 router.get("/complete", function(req, res) {
-  console.log("complete post");
+  console.log("complete post " + req);
 });
 
 router.get("/fullscreen", function(req, res) {
-  console.log("fullscreen get");
+  console.log("fullscreen get " + req);
   res.status(200).send();
 });
 
 router.post("/fullscreen", function(req, res) {
-  console.log("fullscreen post");
+  console.log("fullscreen post " + req);
 });
 
 router.get("/clicked", function(req, res) {
-  console.log("video clicked get");
+  console.log("video clicked get " + req);
   res.status(200).send();
 });
 
 router.post("/clicked", function(req, res) {
-  console.log("video clicked post");
+  console.log("video clicked post " + req);
 });
 
 router.get("/createView", function(req, res) {
-  console.log("video createView get");
+  console.log("video createView get " + req);
   res.status(200).send();
 });
 
 router.post("/createView", function(req, res) {
-  console.log("video createView post");
+  console.log("video createView post " + req);
 });
 
 module.exports = router;
